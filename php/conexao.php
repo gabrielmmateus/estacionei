39b8d6df3d48b3dd2640fe4c3277737262dc7a6d
gabrielmmateus <114250803+gabrielmmateus@users.noreply.github.com>
2023-05-03 14:41:54 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,7 +1,7 @@
 <?php 
-$server = "localhost";
-$user = "root";
-$password = "";
+$server = "db-server-gabrielm.mysql.database.azure.com";
+$user = "admin1";
+$password = "$#nai@1152dt";
 $dbname = "estacionamento";
 
 $con = mysqli_connect($server, $user, $password, $dbname);
